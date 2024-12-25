6761440bb7080af7cf0bd676a00524fc2780118f
name <name@website.com>
2024-12-24 20:32:48 -0500

    Updated to warp for the server to preserve the use of the MIT license.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,5 @@
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
-use serde_json::{Result, Value};
-
 mod game;
 
 fn main() {
-	let listener = TcpListener::bind("127.0.0.1:13337").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request: {http_request:#?}");
-    stream.write_all("HTTP/1.1 200 OK\r\nOK\r\n".as_bytes()).unwrap();
-}
