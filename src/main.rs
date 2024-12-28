62d911681d83935df5ce16668e1eaa011e1f92c5
name <name@website.com>
2024-12-27 23:21:24 -0500

    Saving changes.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use warp::Filter;
 mod game;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let game = game::Game::new();
+
+    // Targets
+    let targets_uri = warp::path("targets"); // /targets
+    let get_targets = targets_uri.and(warp::get())
+        .and(warp::path::param::<usize>())
+        .map(|index| {
+            
+        }); // /targets/<index> - -1 returns all targets
+
+    // Units
+    warp::serve(get_target).run(([127, 0, 0, 1], 8910)).await
 }
 
