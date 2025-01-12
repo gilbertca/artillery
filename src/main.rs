f1a9c0ceb5ad58ff94a0618ea5139ba2471e86c7
name <name@website.com>
2025-01-11 23:39:10 -0500

    Saving changes. Passes compiler.

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,22 @@ mod filters {
     
     /// `with_game` is used internally to include a thread-safe reference to the game's state
     fn with_game(
-        game: Game
-    ) -> impl Filter<Extract = (Game,), Error = std::convert::Infallible> + Clone {
+        game: handlers::Game
+    ) -> impl Filter<Extract = (handlers::Game,), Error = std::convert::Infallible> + Clone {
         warp::any().map(move || game.clone())
     }
 }
 
 mod handlers {
-    use crate::game::Game as _Game;
+    use crate::game::{Game as _Game, ArtilleryError};
     use std::sync::Arc;
     use tokio::sync::Mutex;
     use warp::http::StatusCode;
 
-    type Game = Arc<Mutex<_Game>>;
+    pub type Game = Arc<Mutex<_Game>>;
 
-    pub async fn add_target(_game: Game, x: f32, y: f32) {
-        match game.lock().await.add_target(x, y) {
-            Ok => Ok(StatusCode::
-        }
+    pub async fn add_target(game: Game, x: f32, y: f32) {
+
     }
 
 }
