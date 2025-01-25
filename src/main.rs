c0eaa6d08898bc57a28953a04c78ca35598629b3
name <name@website.com>
2025-01-25 16:49:40 -0500

    Passes compiler.

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,25 @@ use std::collections::HashMap;
 use warp::Filter;
 use std::sync::Arc;
 use tokio::sync::Mutex;
-use game::Game as _Game;
 
-pub type Game = Arc<Mutex<Hashmap>>
-
+mod game;
 
 #[tokio::main]
 async fn main() {
-    // TODO: Save / Load games?
-    let mut games: HashMap<String, Game> = HashMap::new();
+    use game::Game;
+    let mut game = Arc::new(Mutex::new(Game::new()));
 
-    let routes = filters::
 
 }
 
 mod filters {
-    use super::handlers;
     use warp::Filter;
+    use crate::game::Game as _Game;
+    use std::sync::Arc;
+    use tokio::sync::Mutex;
+
+    pub type Game = Arc<Mutex<_Game>>;
 
     
-    fn with_game(
-        game: handlers::Game
-    ) -> impl Filter<Extract = (handlers::Game,), Error = std::convert::Infallible> + Clone {
-        warp::any().map(move || game.clone())
-    }
 }
-
-mod handlers {
-
-    pub async fn add_target(game: Game, x: f32, y: f32) {
-
-    }
-
-}
-
-
 
