3c689b88bd6608d312e45223d623a787060e83fa
Chris Gilbert <chrisagilbert.cg@gmail.com>
2025-08-08 04:45:24 -0400

    Updated server to use strings for all data types

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ type Game = Arc<Mutex<game::Game>>;
 ///
 /// Running this will start a `warp` server on port 10707.
 /// TODO: ArtilleryErrors are serializable, and can be included in `response` HashMaps
-/// TODO: WE CAN RETURN TUPLES RATHER THAN JSON ENUMS WITHIN `handlers`
 ///
 /// All paths either return or accept JSON objects.
 /// URI paths:
@@ -26,7 +25,6 @@ type Game = Arc<Mutex<game::Game>>;
 /// - /game/run POST -> runs the simulation using `Game.run_turn`
 /// TODO: both players must run the game, so the server needs to track the connections or name in
 /// some way. perhaps all functions could simply accept a name to validate.
-/// TODO: UPDATING GAME SETTINGS? /game/settings/ 
 #[tokio::main]
 async fn main() {
     use game::Game;
@@ -220,20 +218,6 @@ mod handlers {
     use crate::game::ArtilleryError::{IndexError, DistanceError, ResourceError};
     use warp::http::{StatusCode, Response};
 
-    // This is required so that we may include different value types within our 'response' HashMaps
-    #[derive(Serialize)]
-    enum JSON {
-        Coordinate(Coordinate),
-        Coordinates(Vec<Coordinate>),
-        F32(f32),
-        F32s(Vec<f32>),
-        Usize(usize),
-        Usizes(Vec<usize>),
-        Bool(bool),
-        Bools(Vec<bool>),
-        Error(ArtilleryError),
-    }
-
     /// *   * **   * ***** ******* ******
     /// *   * * *  *   *      *    **
     /// *   * *  * *   *      *      **
@@ -244,12 +228,12 @@ mod handlers {
     /// Also includes all unit destinations using `Game.get_destinations`
     pub async fn get_all_units(mut game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
         response.insert("positions",
-                        JSON::Coordinates(gamestate.get_units().clone()));
+                        serde_json::to_string(&&gamestate.get_units().clone()).unwrap());
         response.insert("destinations",
-                        JSON::Coordinates(gamestate.get_destinations().clone()));
+                        serde_json::to_string(&&gamestate.get_destinations().clone()).unwrap());
 
         Ok(warp::reply::json(&response))
     }
@@ -258,26 +242,26 @@ mod handlers {
     /// Also includes the unit's destination using `Game.get_destination`
     pub async fn get_unit(index: usize, mut game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
         match gamestate.get_unit(index) {
             Ok(unit) => {
                 response.insert("position",
-                                JSON::Coordinate(unit.clone()));
+                                serde_json::to_string(&unit.clone()).unwrap());
                 response.insert("destination",
-                    JSON::Coordinate(
+                    serde_json::to_string(&
                         gamestate
                         .get_destination(index)
                         .expect(format!("If a unit exists at {}, then a destination must exist at {}", index, index).as_str())
                         .clone()
-                    ),
+                    ).unwrap(),
                 );
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::OK));
             }
             Err(error) => {
                 // `Game.get_unit` currently only fails when a unit doesn't exist at that index
-                response.insert("error", JSON::Error(error));
+                response.insert("error", serde_json::to_string(&error).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::NOT_FOUND));
             }
@@ -288,17 +272,17 @@ mod handlers {
     /// There aren't any rules regarding unit limits; maybe that's the client's job
     pub async fn create_unit(coordinate: Coordinate, game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
         match gamestate.add_unit(coordinate.x, coordinate.y) {
             Ok(_) => {
                 response.insert("coordinate",
-                                JSON::Coordinate(coordinate.clone()));
+                                serde_json::to_string(&coordinate.clone()).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::CREATED));
             },
             Err(error) => { // Currently only fails when unit is outside map, or inside the base
-                response.insert("error", JSON::Error(error));
+                response.insert("error", serde_json::to_string(&error).unwrap());
 
                 Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::BAD_REQUEST))
             }
@@ -308,12 +292,12 @@ mod handlers {
     /// `handlers::delete_unit` deletes a unit at the specified `index` using `Game.remove_unit`
     pub async fn delete_unit(index: usize, game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
         match gamestate.remove_unit(index) {
             Ok(_) => { return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::NO_CONTENT)); },
             Err(error) => { // `Game.remove_unit` currently only fails when index DNE
-                response.insert("error", JSON::Error(error));
+                response.insert("error", serde_json::to_string(&error).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::NOT_FOUND));
             }
@@ -323,17 +307,17 @@ mod handlers {
     /// `handlers::set_destination` sets a unit's destination at a particular `index`
     pub async fn set_destination(index: usize, coordinate: Coordinate, game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
         match gamestate.set_destination(index, coordinate.x, coordinate.y) {
             Ok(_) => {
-                response.insert("index", JSON::Usize(index));
-                response.insert("coordinate", JSON::Coordinate(coordinate));
+                response.insert("index", serde_json::to_string(&index).unwrap());
+                response.insert("coordinate", serde_json::to_string(&coordinate).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::CREATED));
             }
             Err(error) => {
-                response.insert("error", JSON::Error(error));
+                response.insert("error", serde_json::to_string(&error).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::BAD_REQUEST));
             }
@@ -351,10 +335,10 @@ mod handlers {
     /// Also includes the current target costs using `Game.get_target_costs`
     pub async fn get_all_targets(mut game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
-        response.insert("targets", JSON::Coordinates(gamestate.get_targets().clone()));
-        response.insert("target_costs", JSON::F32s(gamestate.get_target_costs().clone()));
+        response.insert("targets", serde_json::to_string(&gamestate.get_targets().clone()).unwrap());
+        response.insert("target_costs", serde_json::to_string(&gamestate.get_target_costs().clone()).unwrap());
 
         Ok(warp::reply::json(&response))
     }
@@ -363,26 +347,26 @@ mod handlers {
     /// `Game.get_target`
     pub async fn get_target(index: usize, mut game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
         match gamestate.get_target(index) {
             Ok(target) => {
-                response.insert("target", JSON::Coordinate(target.clone()));
+                response.insert("target", serde_json::to_string(&target.clone()).unwrap());
                 response.insert(
                     "target_cost",
-                    JSON::F32(
+                    serde_json::to_string(&
                             gamestate
                             .get_target_cost(index)
                             .expect(format!("If a target exists at {}, then a cost must exist at {}", index, index).as_str())
                             .clone()
-                    )
+                    ).unwrap()
                 );
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::CREATED));
             }
             // `Game.get_target` currently only fails when the index DNE
             Err(error) => {
-                response.insert("error", JSON::Error(error));
+                response.insert("error", serde_json::to_string(&error).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::NOT_FOUND));
             }
@@ -393,20 +377,20 @@ mod handlers {
     /// `Game.add_target`
     pub async fn create_target(coordinate: Coordinate, game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
         match gamestate.add_target(coordinate.x, coordinate.y) {
             Ok(_) => { 
                 let coordinate = Coordinate { x: coordinate.x, y: coordinate.y };
-                response.insert("target", JSON::Coordinate(coordinate));
+                response.insert("target", serde_json::to_string(&coordinate).unwrap());
                 let index = gamestate.get_targets().len() - 1;
                 let target_cost = gamestate.get_target_cost(index).expect("Target was just created by `create_target`").clone();
-                response.insert("cost", JSON::F32(target_cost));
+                response.insert("cost", serde_json::to_string(&target_cost).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::CREATED));
             }
             Err(error) => { // Fails when target is out of map, and not enough resources
-                response.insert("error", JSON::Error(error));
+                response.insert("error", serde_json::to_string(&error).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::FORBIDDEN));
             },
@@ -416,12 +400,12 @@ mod handlers {
     /// `handlers::delete_target` deletes the newest target using `Game.remove_newest_target`
     pub async fn delete_target(game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
-        let mut response: HashMap<&str, JSON> = HashMap::new();
+        let mut response: HashMap<&str, String> = HashMap::new();
 
         match gamestate.remove_newest_target() {
             Ok(_) => { return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::NO_CONTENT)); },
             Err(error) => {
-                response.insert("error", JSON::Error(error));
+                response.insert("error", serde_json::to_string(&error).unwrap());
 
                 return Ok(warp::reply::with_status(warp::reply::json(&response), StatusCode::FORBIDDEN));
             }
@@ -439,14 +423,14 @@ mod handlers {
     pub async fn get_game_config(game: Game) -> Result<impl warp::Reply, Infallible> {
         let mut gamestate = game.lock().await;
 
-        let mut response: HashMap<&str, JSON> = HashMap::new();
-        response.insert("map_radius", JSON::F32(gamestate.get_map_radius().clone()));
-        response.insert("target_radius", JSON::F32(gamestate.get_target_radius().clone()));
-        response.insert("base_coords", JSON::Coordinate(gamestate.get_base_coords().clone()));
-        response.insert("base_radius", JSON::F32(gamestate.get_base_radius().clone()));
-        response.insert("max_unit_range", JSON::F32(gamestate.get_max_unit_range().clone()));
-        response.insert("max_resources", JSON::F32(gamestate.get_max_resources().clone()));
-        response.insert("game_over", JSON::Bool(gamestate.get_game_over().clone()));
+        let mut response: HashMap<&str, String> = HashMap::new();
+        response.insert("map_radius", serde_json::to_string(&gamestate.get_map_radius().clone()).unwrap());
+        response.insert("target_radius", serde_json::to_string(&gamestate.get_target_radius().clone()).unwrap());
+        response.insert("base_coords", serde_json::to_string(&gamestate.get_base_coords().clone()).unwrap());
+        response.insert("base_radius", serde_json::to_string(&gamestate.get_base_radius().clone()).unwrap());
+        response.insert("max_unit_range", serde_json::to_string(&gamestate.get_max_unit_range().clone()).unwrap());
+        response.insert("max_resources", serde_json::to_string(&gamestate.get_max_resources().clone()).unwrap());
+        response.insert("game_over", serde_json::to_string(&gamestate.get_game_over().clone()).unwrap());
 
         Ok(warp::reply::json(&response))
     }
--- turtle-client.py
+++ turtle-client.py
@@ -2,4 +2,47 @@ import requests
 import argparse
 from turtle import Turtle
 
+# Globals
+URL = None
 
+class Unit:
+    def __init__(self, x, y):
+        self.pos = (x, y)
+        self.des = (x, y)
+        self.t = Turtle()
+        self.t.color('blue')
+
+
+class Target:
+    def __init__(self, x, y, cost):
+        self.pos = (x, y)
+        self.cost = cost
+        self.t = Turtle()
+        self.t.color('red')
+
+
+class Game:
+    def __init__(self):
+        self.units = []
+        self.targets = []
+        self.map_radius = 0
+
+    def run(self):
+        # Draw the map:
+        pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description='A client written with Python\'s turtle for the Artillery game.')
+    parser.add_argument('--url', type=str, required=True)
+    args = parser.parse_args()
+    URL = args.url
+    game = Game()
+    while True:
+        try:
+            game.run()
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(e)
+    print(requests.get(f"{URL}/units").json())
