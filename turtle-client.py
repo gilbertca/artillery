2be3e297c5828f50f0563cddd081c72157912906
Chris Gilbert <chrisagilbert.cg@gmail.com>
2025-08-09 14:52:45 -0400

    Saving changes; Unit side is almost able to place units.

--- turtle-client.py
+++ turtle-client.py
@@ -10,7 +10,7 @@ BG_COLOR = "#E5C287"
 turtle.bgcolor(BG_COLOR)
 # Globals
 URL = None
-MAX_EXCEPTIONS = 3
+SCALE = 3
 UNIT_COLOR = 'blue'
 TARGET_COLOR = 'red'
 MAP_COLOR = 'black'
@@ -46,16 +46,10 @@ class Game:
         self.turtles.append(t)
         return t
 
-    def clear_turtle(self, index):
-        t = self.turtles.pop(index)
-        t.reset()
-        t.hideturtle()
-
     def clear_turtles(self):
         while len(self.turtles) != 0:
             t = self.turtles.pop()
-            t.reset()
-            t.hideturtle()
+            self._del_turtle(t)
 
     def _del_turtle(self, turtle):
             # Unfortunately, there is no clean way to *delete* a turtle from the canvas
@@ -72,63 +66,84 @@ class Game:
             self.game_settings.update({key: json.loads(value)})
 
     def get_player_side(self):
-        while self.chosen_side == 0:
-            # Clicking a turtle chooses the side
-            unit_turtle = self.Turtle()
-            target_turtle = self.Turtle()
-            # DRY setup:
-            for t in ((unit_turtle, -100, 0, UNIT_COLOR, self._pick_unit_side, "Choose unit side"),
-                      (target_turtle, 100, 0, TARGET_COLOR, self._pick_target_side, "Choose target side")):
-                i = t[0] # i = instance
-                i.hideturtle() # From the doc: "hiding the turtle speeds up the drawing observably"
-                i.penup()
-                i.shapesize(2, 4, 1)
-                i.shape("square")
-                i.goto(t[1], t[2])
-                i.color(t[3])
-                i.onclick(t[4])
-                i.write(t[5])
-                i.goto(t[1], -20)
-                i.showturtle() # Reveal turtle
-
-            # To create a non-blocking idle mode, we draw a circle (with a bit of magic to speed it up)
-            t = self.Turtle()
-            t.speed("fast")
-            t.penup()
-            t.forward(200)
-            t.left(90)
-            t.pendown()
-            drawn = False
-            while self.chosen_side == 0:
-                if drawn: # the magic
-                    t.speed("fastest")
-                t.circle(200)
-                drawn = True
-        # Before control is returned to `self.run`, we cleanup:
-        self.clear_turtles()
+        # Clicking a turtle chooses the side
+        unit_turtle = self.Turtle()
+        target_turtle = self.Turtle()
+        # DRY setup:
+        for t in ((unit_turtle, -100, 0, UNIT_COLOR, self._pick_unit_side, "Choose unit side"),
+                  (target_turtle, 100, 0, TARGET_COLOR, self._pick_target_side, "Choose target side")):
+            i = t[0] # i = instance
+            i.hideturtle() # From the doc: "hiding the turtle speeds up the drawing observably"
+            i.penup()
+            i.shapesize(2, 4, 1)
+            i.shape("square")
+            i.goto(t[1], t[2])
+            i.color(t[3])
+            i.onclick(t[4])
+            i.write(t[5])
+            i.goto(t[1], -20)
+            i.showturtle() # Reveal turtle
 
     def _pick_unit_side(self, x, y):
         # self.chosen_side = 1 represents unit side
         self.chosen_side = 1
+        self.clear_turtles()
+        self.play()
 
     def _pick_target_side(self, x, y):
         # self.chosen_side = 2 represents target side
         self.chosen_side = 2
+        self.clear_turtles()
+        self.play()
 
-    def draw_map(self, chosen_side):
-        pass
+    def draw_map(self):
+        self.map_turtle = self.Turtle()
+        self.map_turtle.speed(0)
+        self._draw_circle(self.game_settings['map_radius'], "black") # Draw map
+        self._draw_circle(self.game_settings['base_radius'], "magenta") # Draw base
+        self.map_turtle.hideturtle()
+
+    def _draw_circle(self, radius, color):
+        self.map_turtle.hideturtle()
+        self.map_turtle.penup()
+        self.map_turtle.goto(0, 0)
+        self.map_turtle.forward(SCALE*radius)
+        self.map_turtle.left(90)
+        self.map_turtle.pendown()
+        self.map_turtle.color(color)
+        self.map_turtle.showturtle()
+        self.map_turtle.circle(SCALE*radius)
+
+    def place_unit(self, x, y):
+        x = x / SCALE
+        y = y / SCALE
+        response = requests.post(f"{URL}/units", json={'x': x, 'y': y})
+        print(response.json())
 
     def play(self):
-        while True:
-            # Draw play area:
-            self.draw_map(self.chosen_side)
+        self.draw_map()
+        if self.chosen_side == 1: # Unit side
+            self.unit_turtle = self.Turtle()
+            self.unit_turtle.hideturtle()
+            self.unit_turtle.penup()
+            self.unit_turtle.left(90)
+            self.unit_turtle.forward(SCALE * (self.game_settings['map_radius'] + 20))
+            self.unit_turtle.write("Click and drag to place the unit on the map")
+            self.unit_turtle.backward(20)
+            self.unit_turtle.showturtle()
+            self.unit_turtle.ondrag(self.unit_turtle.goto)
+            self.unit_turtle.onrelease(self.place_unit)
+        elif self.chosen_side == 2: # Artillery side
+            pass
 
     def run(self):
         # Begin by updating game's settings (they might change between 'runs')
         self.get_game_settings()
         # Ask the user to choose a side:
         self.get_player_side()
-        self.play()
+        # Begin the main loop:
+        # see: https://stackoverflow.com/questions/39429227/turtle-graphics-python-mainloop/41205497#41205497
+        turtle.mainloop()
 
 if __name__ == "__main__":
     # Setup arguments:
@@ -141,23 +156,5 @@ if __name__ == "__main__":
     # Create game, main loop:
     game = Game()
     exception_count = 0
-    while True:
-        # Attempt game
-        try:
-            game.run()
-        # Only quit on KeyboardInterrupt (Ctrl+C)
-        except KeyboardInterrupt:
-            break
-        # Print all other exceptions, quit after trying MAX_EXCEPTIONS times
-        except Exception as e:
-            print(e)
-            if exception_count >= MAX_EXCEPTIONS:
-                print(f"""
-
-The program has encountered the maximum number of allowed,
-unhandled exceptions ({MAX_EXCEPTIONS}). The program will now quit.
-
-                """)
-                break
-            exception_count += 1
+    game.run()
 
