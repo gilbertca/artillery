324153e6d19d34267642681fa5564e309bd1a30a
Chris Gilbert <chrisagilbert.cg@gmail.com>
2025-09-09 20:05:03 -0400

    Created a proper module hierarchy for the Python clients

---
+++ py/drawing/extended_turtles.py
@@ -0,0 +1,23 @@
+"""**extended_turtles** is a collection of classes which extend the functionality of Python's `turtle.Turtle` class.
+"""
+from turtle import Turtle
+
+class ButtonTurtle(Turtle):
+    """Provides `onclick` functionality to a turtle."""
+    ...
+
+
+class UnitTurtle(Turtle):
+    ...
+
+
+class DestinationTurtle(Turtle):
+    ...
+
+
+class TargetTurtle(Turtle):
+    ...
+
+
+class ShapeTurtle(Turtle):
+    ...
